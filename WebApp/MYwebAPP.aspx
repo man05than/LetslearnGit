04663493996ae62eda4478e6333b7e1c225984d8
Rishikesh Verma <rishikeshverma20@gmail.com>
2022-05-13 16:16:45 +0530

    adding webapp

---
+++ WebApp/MYwebAPP.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MYwebAPP.aspx.cs" Inherits="Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <telerik:RadStyleSheetManager id="RadStyleSheetManager1" runat="server" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+        <Scripts>
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+            <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+        </Scripts>
+    </telerik:RadScriptManager>
+    <script type="text/javascript">
+        //Put your JavaScript code here.
+    </script>
+    <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+    </telerik:RadAjaxManager>
+    <div>
+        <table>
+            <tr>
+                <td >
+                     <telerik:RadDropDownList ID="ddlContinent" width ="200" runat ="server"
+                         AutoPostBack ="true" OnSelectedIndexChanged ="ddlcontinent_SelectedIndexChanged">
+        </telerik:RadDropDownList>
+                     <br />
+                </td>
+            </tr>
+         <tr>
+             <td>
+                 <telerik:RadDropDownList ID="ddlCountries"  width ="200" runat ="server"
+                     OnSelectedIndexChanged ="ddlCountries_SelectedIndexChanged"
+                     AutoPostBack ="true" >
+        </telerik:RadDropDownList>
+                 <br />
+             </td>
+         </tr>
+            <tr>
+            <td>
+                     <telerik:RadDropDownList ID="cboCity" runat= "server" width ="200" 
+                         AutoPostBack ="true" >
+                     </telerik:RadDropDownList>
+            </td>
+            </tr>
+                
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApp/MYwebAPP.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+           ddlContinent.DataSource = GetData("sp_getcontinent", null);
+            ddlContinent.DataTextField = "ContinentName";
+            ddlContinent.DataValueField = "ContinentID";
+            ddlContinent.DataBind();
+            ddlContinent.Items.Insert(0, new DropDownListItem("Select Continent", "-1"));
+            ddlCountries.Items.Insert(0, new DropDownListItem("Select Countries", "-1"));
+            cboCity.Items.Insert(0, new DropDownListItem("Select City", "-1"));
+
+            ddlCountries.Enabled = false;
+            cboCity.Enabled = false;
+        }
+    }
+
+    private DataSet GetData(string spName, SqlParameter spParameter)
+    {
+
+        DataSet das = new DataSet();
+        string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            SqlDataAdapter da = new SqlDataAdapter(spName, con);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            if(spParameter!= null)
+            {
+                da.SelectCommand.Parameters.Add(spParameter);
+            }
+
+            da.Fill(das );
+        }
+        return das ;
+
+
+    }
+
+
+
+    protected void ddlcontinent_SelectedIndexChanged(object sender, DropDownListEventArgs e)
+    {
+        if(ddlContinent.SelectedIndex == 0)
+        {
+            ddlCountries.SelectedIndex = 0;
+            ddlCountries.Enabled = false;
+            cboCity.SelectedIndex = 0;
+            cboCity.Enabled = false;
+        }
+        else
+        {
+            ddlCountries.Enabled = true;
+            SqlParameter spparam = new SqlParameter("@ContinentID", ddlContinent.SelectedValue);
+            ddlCountries.DataSource = GetData("sp_getcountriesbycontinent", spparam);
+            ddlCountries.DataTextField = "CountriesName";
+            ddlCountries.DataValueField = "CountriesID";
+            ddlCountries.DataBind();
+            ddlCountries.Items.Insert(0, new DropDownListItem("Select Countries", "-1"));
+
+        }
+    }
+
+    protected void ddlCountries_SelectedIndexChanged(object sender, DropDownListEventArgs e)
+    {
+        if (ddlCountries.SelectedIndex == 0)
+        {
+            cboCity.SelectedIndex = 0;
+            cboCity.Enabled = false;
+        }
+        else
+        {
+            cboCity.Enabled = true;
+
+            SqlParameter spparam = new SqlParameter("@CountryID", ddlCountries.SelectedValue);
+            cboCity.DataSource = GetData("sp_getcitybycountry", spparam);
+            cboCity.DataTextField = "CityName";
+            cboCity.DataValueField = "CityID";
+            cboCity.DataBind();
+            cboCity.Items.Insert(0, new DropDownListItem("Select City", "-1"));
+
+        }
+    }
+}
